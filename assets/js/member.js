cef3bdbc0d715db42db556e23442b98ebc2127ca
XiangLi1412 <63719557+XiangLi1412@users.noreply.github.com>
2020-06-20 22:44:29 +0800

    Add files via upload

---
+++ assets/js/member.js
@@ -0,0 +1,25 @@
+function toMember2(){
+	window.location.href = "manage_member2.html";
+	console.log("test");
+}
+
+function toGood2(){
+	window.location.href = "manage_good2.html";
+	console.log("test");
+}
+
+function toOrder2(){
+	window.location.href = "manage_order2.html";
+	console.log("test");
+}
+
+function toSale2(){
+	window.location.href = "manage_sale2.html";
+	console.log("test");
+}
+
+function toComment2(){
+	window.location.href = "manage_comment2.html";
+	console.log("test");
+}
+
